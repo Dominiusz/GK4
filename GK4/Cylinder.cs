4d40532a3859a0bc3bf942bf9748b95a5296d3c6
Dominiusz <dominik.strojek@gmail.com>
2018-01-13 00:25:16 +0100

    +Cylinder

---
+++ GK4/Cylinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GK4
+{
+    class Cylinder
+    {
+        public Triangle[] triangles;
+
+        public Cylinder(int pieces)
+        {
+            if(pieces<4)
+                throw new ArgumentException();
+
+
+        }
+
+    }
+}
